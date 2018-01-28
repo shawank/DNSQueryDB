2d6a57e834052dfa7e202b3f455434a7e13eb345
Ankush Shaw <shawank17198@gmail.com>
2018-01-28 14:22:37 +0530

    script completed for simple csv query

---
+++ query_csv.py
@@ -0,0 +1,50 @@
+import csv
+import subprocess
+import re
+
+with open('top-1m.csv', 'r+') as fr:
+    with open('database.csv', 'w+') as fw:
+        domain = fr.readline()
+        temp = re.findall(r',([\w\.]+)', domain)
+        domain = temp[0]
+        writer = csv.writer(fw, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['Domain Queried', 'Answer Type', 'Domain Name Returned', 'TTL', 'Unknown', 'Query Type', 'IP address'])
+        j = 1
+        while (domain):
+            result = subprocess.run(['dig', '+nocmd', '+noquestion', 'ANY', domain], stdout = subprocess.PIPE)
+            str = result.stdout.decode("UTF-8")
+            # pos_answer = findnth(str,"ANSWER", 1)
+            # # print(pos_answer)
+            # str_temp = str
+            # pos_authority = findnth(str, "AUTHORITY", 1, offset = 103)
+            # # print(pos_authority)
+            # pos_additional = findnth(str, "ADDITIONAL", 1, offset = pos_authority)
+            # # regex = re.compile(r'SECTION:\s+()')
+            match = re.findall(r'.+(?!SECTION)', str)
+            length = len(match)
+            i = 0
+            while (i < length):
+                if (re.search(r':$', match[i])):
+                    temp = re.search(r'(\w+):$', match[i])
+                    if (temp.groups(1) == 'answer'):
+                        break
+                    
+                    
+                    element = re.findall(r'([\w\s]*):', match[i])
+                    answer_type = element[0]
+                    i += 1
+                    while(re.search(r'^[^;]', match[i]) != None):
+                        element = re.findall(r'([\w+\.:]+)\.?\t?', match[i])
+
+                        element = [j] + [domain] + [answer_type] + element
+                        writer.writerow(element)
+                        i += 1
+                        if (i == length):
+                            break
+                    i -= 1
+
+                i += 1
+            j += 1
+            domain = fr.readline()
+            temp = re.findall(r',([\w\.]+)', domain)
+            domain = temp[0]
